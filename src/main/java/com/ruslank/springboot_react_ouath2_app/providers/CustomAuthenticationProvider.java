3eead7630ecc72846d7173569fd5d039f8808128
Ruslan Kultaev <ruslankultaev7@gmail.com>
2023-04-30 14:02:04 +0200

    implemented methods by UserDetailService interface and provided some logic in loadByUsername

---
+++ src/main/java/com/ruslank/springboot_react_ouath2_app/providers/CustomAuthenticationProvider.java
@@ -0,0 +1,4 @@
+package com.ruslank.springboot_react_ouath2_app.providers;
+
+public class CustomAuthenticationProvider {
+}
--- src/main/java/com/ruslank/springboot_react_ouath2_app/services/CustomUserDetailsService.java
+++ src/main/java/com/ruslank/springboot_react_ouath2_app/services/CustomUserDetailsService.java
@@ -1,4 +1,29 @@
 package com.ruslank.springboot_react_ouath2_app.services;
 
-public class CustomUserDetailsService {
+import com.ruslank.springboot_react_ouath2_app.entities.User;
+import com.ruslank.springboot_react_ouath2_app.implementations.CustomUserDetails;
+import com.ruslank.springboot_react_ouath2_app.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Supplier<UsernameNotFoundException> supplier =
+                () -> new UsernameNotFoundException("authentication issue!");
+        Optional<User> user = this.userRepository.findByUsername(username);
+        return user.map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("username was not found"));
+    }
 }
